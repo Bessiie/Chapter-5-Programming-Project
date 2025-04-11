ef6d2aa2cfb6aa65817b8c52dcbbe858ee6238f8
Jesse zheng <BessiieBoi@outlook.com>
2025-04-10 21:52:21 -0400

    Version 1

--- Chapter 5 Programming Project/Chapter 5 Programming Project.cpp
+++ Chapter 5 Programming Project/Chapter 5 Programming Project.cpp
@@ -1,20 +1,37 @@
-// Chapter 5 Programming Project.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// A teacher has asked all her students to line up according to their first name.
+// For example, in one class Amy will be at the front of the line, and Yolanda will be at the end. 
+// Write a program that reads the student names from the file LineUp.txt. 
+// The program should read names from the file until there is no more data to read.
+// Once all the names have been read, it reports the number of students in the class, which student would be at the front of the line,
+// and which one would be at the end of the line.
+// You may assume that no two students have the same name.
 
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+int main() {
+    std::ifstream fileLineUp("LineUp.txt");  // Declare and open the file
+
+    if (!fileLineUp) {
+        std::cout << "Error opening file!" << std::endl;
+        return 1;  // Error code if file cannot be opened
+    }
+
+    std::vector<std::string> studentLineUp;  // Vector to store student names
+    std::string nameStudent; // Student name
+    
+    //Reads file for names and places them into a vector
+    while (std::getline(fileLineUp, nameStudent)) { 
+        studentLineUp.push_back(nameStudent);
+    }
+
+    fileLineUp.close();  //Closes file
+
+    std::cout << "Total number of students: " << studentLineUp.size() << std::endl;
+    std::cout << "The student at the front of the line: " << studentLineUp.front() << std::endl;
+    std::cout << "The student at the end of the line: " << studentLineUp.back() << std::endl;
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
