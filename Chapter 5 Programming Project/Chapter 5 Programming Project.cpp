1e1925fdc0ac9dc1ec17506057fd3efe3d5ec06b
Jesse zheng <BessiieBoi@outlook.com>
2025-04-10 23:08:09 -0400

    Version 1

--- Chapter 5 Programming Project/Chapter 5 Programming Project.cpp
+++ Chapter 5 Programming Project/Chapter 5 Programming Project.cpp
@@ -1,20 +1,25 @@
-// Chapter 5 Programming Project.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+int main() {
+    // Pattern A: Increasing
+    std::cout << "Pattern A:\n";
+    //Determines the amount of rows to print
+    for (int row = 1; row <= 10; ++row) {
+        //Determines the amount of + to print
+        for (int starAmount = 0; starAmount < row; ++starAmount) {
+            std::cout << "+";
+        }
+        std::cout << std::endl;
+    }
+
+    // Pattern B: Decreasing
+    std::cout << "\nPattern B:\n";
+    for (int row = 10; row >= 1; --row) {
+        for (int starAmount = 0; starAmount < row; ++starAmount) {
+            std::cout << "+";
+        }
+        std::cout << std::endl;
+    }
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
